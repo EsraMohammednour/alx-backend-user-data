5b24e949ebb46755c26b140f0c9375249724d190
esraa <esraa2023112@gmail.com>
2024-11-07 20:13:06 +0200

    done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,13 +2,14 @@
 '''filtered_logger.py'''
 import re
 from typing import List
-
-
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
 }
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str) -> str:
     '''function called filter_datum that returns the log message'''
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
