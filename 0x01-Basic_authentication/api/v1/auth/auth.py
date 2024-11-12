e93e00908a3b086e6af503a6f4fb9a5916f2824a
esraa <esraa2023112@gmail.com>
2024-11-12 16:58:37 +0200

    done

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,6 +11,13 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''Method to check if a given path requires authentication
         '''
+        if path is None :
+            return True
+        elif excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for excluded_path in excluded_paths:
+            if path.rstrip('/') == excluded_path.rstrip('/'):
+                return False
         return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
