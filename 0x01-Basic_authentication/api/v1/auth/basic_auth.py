986d3f6f48b308cbf119e430e619f57a3a87a479
esraa <esraa2023112@gmail.com>
2024-11-13 21:50:08 +0200

    done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 '''module for crating new class'''
 from .auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     '''creating empty class'''
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        '''return Base64 authorization header'''
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
+
