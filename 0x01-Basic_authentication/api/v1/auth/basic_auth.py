39df7d0dc413ea63f8674c74a6a9806b95793f6e
esraa <esraa2023112@gmail.com>
2024-11-24 22:27:41 +0200

    done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,15 @@ class BasicAuth(Auth):
 
             except (binascii.Error, UnicodeDecodeError):
                 return None
+
+    def extract_user_credentials(
+                                self,
+                                decoded_base64_authorization_header:
+                                str) -> (str, str):
+        if decoded_base64_authorization_header is None:
+            return None, None
+        elif not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        elif ":" not in decoded_base64_authorization_header:
+            return None, None
+        return decoded_base64_authorization_header.split(':')
